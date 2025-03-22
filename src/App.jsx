032fb00cb40f6888fd28e72b2c1e1f03ab23ca8a
Nikhil-2x <nikhilyadav101513@gmail.com>
2025-03-22 15:15:59 +0530

    add MovieCard-Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import Search from './components/Search';
 import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -70,26 +71,26 @@ function App() {
           <header>
             <img src="./hero.png" alt="Hero Banner" />
             <h1>
-              Search the <span className="text-gradient">Movies </span>You'll Enjoy
-              Without the Hassle
+              Search the <span className="text-gradient">Movies </span>You'll
+              Enjoy Without the Hassle
             </h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
 
-          <section className='all-movies'>
-           <h2 className='mt-[40px]'>All Movies</h2> 
-           
-           {isLoading ? (
-            <Spinner/>
-           ) : errormessage ? (
-            <p className='text-red-500'>{errormessage}</p>
-           ) : (
-            <ul>
-              {movieList.map((movie)=>(
-                <p key={movie.id} className='text-white'>{movie.title}</p>
-              ))}
-            </ul>
-           )}
+          <section className="all-movies">
+            <h2 className="mt-[40px] text-gradient">Movies</h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errormessage ? (
+              <p className="text-red-500">{errormessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
           </section>
         </div>
       </main>
